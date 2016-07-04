21f1a5f242108d4e919395e3251a00d0e6f3bd81
Thiago Retondar <t.retondar@gmail.com>
2016-07-04 19:21:10 -0300

    Making pretty awesome button :)

--- app/src/main/java/com/thiagoretondar/addressbook/FormActivity.java
+++ app/src/main/java/com/thiagoretondar/addressbook/FormActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -24,6 +26,16 @@ public class FormActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button saveButton = (Button) findViewById(R.id.form_btn_save);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormActivity.this, "Save Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/thiagoretondar/addressbook/StudentListActivity.java
+++ app/src/main/java/com/thiagoretondar/addressbook/StudentListActivity.java
@@ -1,11 +1,8 @@
 package com.thiagoretondar.addressbook;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,16 +15,18 @@ public class StudentListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
-        String[] students = {"Thiago", "Tiffany", "Felipe", "Paulo"};
+        String[] students = {"Thiago", "Tiffany", "Felipe", "Paulo", "Thiago", "Tiffany", "Felipe",
+                "Paulo", "Thiago", "Tiffany", "Felipe", "Paulo", "Thiago", "Tiffany", "Felipe", "Paulo"};
+
         ListView studentList = (ListView) findViewById(R.id.student_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students);
         studentList.setAdapter(adapter);
