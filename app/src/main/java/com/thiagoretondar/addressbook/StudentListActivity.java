0890ab6f7470decaebbbad4a0d7f83d5df286181
Thiago Retondar <t.retondar@gmail.com>
2016-07-04 13:05:37 -0300

    Using linearLayout to list and adapter to send the information =)

--- app/src/main/java/com/thiagoretondar/addressbook/StudentListActivity.java
+++ app/src/main/java/com/thiagoretondar/addressbook/StudentListActivity.java
@@ -6,8 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentListActivity extends AppCompatActivity {
 
@@ -26,27 +26,11 @@ public class StudentListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String[] students = {"Thiago", "Tiffany", "Felipe", "Paulo"};
+        ListView studentList = (ListView) findViewById(R.id.student_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students);
+        studentList.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_student_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
